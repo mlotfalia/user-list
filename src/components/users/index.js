8ad98dcb9b2652aef001e4e51b1eefd87d97223d
mlotfalia <mahsa.lotfalia@gmail.com>
2022-05-14 17:48:39 +0430

    create form

--- src/components/users/index.js
+++ src/components/users/index.js
@@ -1,23 +1,18 @@
 import AddUser from "./addUser";
 import TableUser from "./table";
+
 import { useState } from "react";
 
-import Header from "./header";
 export default function UsersPage() {
-  const [showModal, setShowModal] = useState(false);
-  const [user, setUser] = useState([]);
+  let [user, setUser] = useState({});
   return (
     <>
       <div className="container   m-4">
-  
-          <Header setShow={setShowModal} />
-          <AddUser addUser={setUser} show={showModal} setShow={setShowModal} />
-
-        </div>
-        <div className="flex justify-center">
-          <TableUser />
-        </div>
-      
+        <AddUser addUser={setUser} />
+      </div>
+      <div className="flex justify-center">
+        <TableUser users={user} />
+      </div>
     </>
   );
 }
---
+++ src/components/users/table.jsx
@@ -0,0 +1,61 @@
+import { AiFillDelete } from "react-icons/ai";
+import { FaEdit } from "react-icons/fa";
+export default function Table({ users }) {
+  return (
+    <>
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table className="w-full  text-gray-500 ">
+          <thead className=" text-s text-gray-700  bg-gray-50 ">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                <span className="sr-only"></span>
+              </th>
+
+              <th scope="col" className="px-6 py-3  text-right">
+                تاریخ عضویت
+              </th>
+
+              <th scope="col" className="px-6 py-3  text-right">
+                نوع عضویت
+              </th>
+              <th scope="col" className="px-6 py-3  text-right">
+                ایمیل
+              </th>
+              <th scope="col" className="px-6 py-3  text-right">
+                تلفن
+              </th>
+              <th scope="col" className="px-6 py-3 text-center">
+                نام کاربر
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.length ? (
+              <tr
+                key={users.email}
+                className="bg-white border-b  hover:bg-gray-50 dark:hover:bg-gray-600"
+              >
+                <td className="px-6 py-4 flex justify-evenly">
+                  <button className=" mr-4 font-medium text-red-600 hover:underline text-right">
+                    <AiFillDelete />
+                  </button>
+                  <a href="#" className="text-indigo-600 hover:text-indigo-900">
+                    <FaEdit />
+                    <span className="sr-only">{users.name}</span>
+                  </a>
+                </td>
+                <td className="px-6 py-4  text-right">{users.name}</td>
+                <td className="px-6 py-4  text-right">{users.tel}</td>
+                <td className="px-6 py-4  text-right">{users.email}</td>
+                <td className="px-6 py-4  text-right">{users.membership}</td>
+                <td className="px-6 py-4  text-right">{users.date}</td>
+              </tr>
+            ) : (
+              " کاربری  در لیست موجود نیست"
+            )}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
